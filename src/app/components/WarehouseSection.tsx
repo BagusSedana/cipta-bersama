83c2a36d41cebf36c9e319765da79e89b59f116b
BagusSedana <gedebagussedanayoga28@gmail.com>
2026-03-15 16:27:36 +0800

    chore: update map location name to Cipta Bersama

--- src/app/components/WarehouseSection.tsx
+++ src/app/components/WarehouseSection.tsx
@@ -95,8 +95,8 @@ export function WarehouseSection() {
 
       <div className="w-full h-[50vh] min-h-[400px] border-t border-gray-100 grayscale hover:grayscale-0 transition-all duration-700">
         <iframe 
-          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=House+of+Rily,+Jl.+Benda+Raya+Kav.+DPR+No.49,+RT.7/RW.11,+Ciganjur,+Kec.+Jagakarsa,+Kota+Jakarta+Selatan,+Daerah+Khusus+Ibukota+Jakarta+12630&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" 
-          title="Lokasi Gudang Cipta Bersama - House of Rily"
+          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Cipta+Bersama,+Jl.+Benda+Raya+Kav.+DPR+No.49,+RT.7/RW.11,+Ciganjur,+Kec.+Jagakarsa,+Kota+Jakarta+Selatan,+Daerah+Khusus+Ibukota+Jakarta+12630&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" 
+          title="Lokasi Gudang Cipta Bersama"
           width="100%" 
           height="100%" 
           style={{ border: 0 }} 
